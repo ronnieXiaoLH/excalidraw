3bdb432f18e3d44b091a4c39635aefe36fd509ad
xiaolonghao <xiaolonghao@linklogis.com>
2024-03-01 10:38:26 +0800

    feat: 修改ws服务地址

--- assets/selectAll-CORXDxkd.js
+++
@@ -1,2 +0,0 @@
-import{a0 as t,a1 as n,a2 as o}from"./index-Zz_m1RtZ.js";function r(e){return typeof e=="string"?new t([document.querySelectorAll(e)],[document.documentElement]):new t([o(e)],n)}export{r as s};
-//# sourceMappingURL=selectAll-CORXDxkd.js.map
